c3616c5098ea16f4ae35c1626cd55195a415721d
zainou.abd <zainoudine.abdallah@outlook.fr>
2020-04-10 16:58:39 +0200

    TP

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AddMuseumComponent } from './add-museum/add-museum.component';
+import { AppComponent } from './app.component';
+import { MuseumService } from './services/openmuseum.service'
+import { SingleMuseumComponent } from './single-museum/single-museum.component';
+import { IndexComponent } from './index/index.component';
+import { MuseumDetailComponent } from './museum-detail/museum-detail.component';
+
+
+const appRoutes: Routes = [
+  { path: 'add', component: AddMuseumComponent },
+  { path: 'museumDetail', component: MuseumDetailComponent },
+
+{ path: '', component: IndexComponent },
+
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    SingleMuseumComponent,
+    IndexComponent,
+    AddMuseumComponent,
+    MuseumDetailComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [MuseumService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
